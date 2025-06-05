a646389eb7769d494452209d02f60e389cd8d3a6
son <son@codegym.vn>
2025-06-05 15:29:03 +0700

    .

--- js/config.js
+++ js/config.js
@@ -1,16 +1,18 @@
+// js/config.js
+
 export const SCHEDULER_RUN_INTERVAL_MS = 25.0
 export const AUDIO_SCHEDULE_LOOKAHEAD_SECONDS = 0.1
+export const SENSITIVITY_FACTOR = 3
 export const TICK_INITIAL_TOP_OFFSET = 5
 export const TICK_LARGE_WIDTH = 1.5
 export const TICK_LARGE_HEIGHT = 10
 export const TICK_SMALL_WIDTH = 1
 export const TICK_SMALL_HEIGHT = 6
 
-export const ANGLE_FOR_0_BPM_MARK = 0     // Góc cho vạch "0 BPM" (12 giờ)
-export const BPM_VALUE_AT_0_DEG_MARK = 0  // Giá trị BPM tượng trưng cho vạch 0 độ
+export const MIN_SCALE_BPM = 40 // BPM nhỏ nhất trên thang đo chính của dial
+export const MAX_SCALE_BPM = 200 // BPM lớn nhất trên thang đo chính của dial
 
-export const ANGLE_FOR_40_BPM_MARK = 30   // Góc cho vạch 40 BPM (ví dụ: 48 độ)
-export const BPM_VALUE_AT_40_DEG_MARK = 40 // Giá trị BPM tại mốc này
-
-export const ANGLE_FOR_200_BPM_MARK = 240 // Góc cho vạch 200 BPM (ví dụ: 240 độ)
-export const BPM_VALUE_AT_200_DEG_MARK = 200 // Giá trị BPM tại mốc này
+export const ANGLE_FOR_0_BPM_MARK = 0     // Vạch "0" tượng trưng luôn ở 12 giờ (0 độ)
+export const BPM_VALUE_FOR_0_DEG_MARK = 0  // Giá trị BPM tượng trưng cho nhãn "0"
+export const ANGLE_FOR_MIN_SCALE_BPM_MARK = 48   // Ví dụ: 48 độ từ vạch "0"
+export const ANGLE_FOR_MAX_SCALE_BPM_MARK = 240 // Ví dụ: 240 độ từ vạch "0"
