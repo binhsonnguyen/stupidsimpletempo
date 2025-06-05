23562da0615b739fcb6007e31f893db88b621418
son <son@codegym.vn>
2025-06-05 15:20:38 +0700

    .

--- js/config.js
+++ js/config.js
@@ -1,9 +1,16 @@
 export const SCHEDULER_RUN_INTERVAL_MS = 25.0
 export const AUDIO_SCHEDULE_LOOKAHEAD_SECONDS = 0.1
-export const DEGREES_PER_BPM_VISUAL = 1.2
-export const SENSITIVITY_FACTOR = 3 // Độ nhạy của vòng xoay BPM
-export const TICK_INITIAL_TOP_OFFSET = 5 // px
-export const TICK_LARGE_WIDTH = 1.5 // px
-export const TICK_LARGE_HEIGHT = 10 // px
-export const TICK_SMALL_WIDTH = 1 // px
-export const TICK_SMALL_HEIGHT = 6 // px
+export const TICK_INITIAL_TOP_OFFSET = 5
+export const TICK_LARGE_WIDTH = 1.5
+export const TICK_LARGE_HEIGHT = 10
+export const TICK_SMALL_WIDTH = 1
+export const TICK_SMALL_HEIGHT = 6
+
+export const ANGLE_FOR_0_BPM_MARK = 0     // Góc cho vạch "0 BPM" (12 giờ)
+export const BPM_VALUE_AT_0_DEG_MARK = 0  // Giá trị BPM tượng trưng cho vạch 0 độ
+
+export const ANGLE_FOR_40_BPM_MARK = 30   // Góc cho vạch 40 BPM (ví dụ: 48 độ)
+export const BPM_VALUE_AT_40_DEG_MARK = 40 // Giá trị BPM tại mốc này
+
+export const ANGLE_FOR_200_BPM_MARK = 240 // Góc cho vạch 200 BPM (ví dụ: 240 độ)
+export const BPM_VALUE_AT_200_DEG_MARK = 200 // Giá trị BPM tại mốc này
