270a5b0e6faa943801a0177f388f3e05192cb062
son <son@codegym.vn>
2025-06-05 15:20:53 +0700

    .

--- js/state.js
+++ js/state.js
@@ -2,7 +2,9 @@ export let currentBpm = 120
 export const minBpm = 20
 export const maxBpm = 300
 export let isMetronomeRunning = false
-export let currentDialRotation = 0 // Thêm trạng thái góc xoay trực quan
+
+export let currentDialRotation = 0 // Góc xoay vật lý của dial
+export let effectiveKnobAngleOnDialScale = 0 // Góc (0-359) trên thang đo dial mà kim đang chỉ vào
 
 export function setCurrentBpm(newBpm) {
     currentBpm = Math.max(minBpm, Math.min(maxBpm, newBpm))
@@ -13,5 +15,11 @@ export function setIsMetronomeRunning(running) {
 }
 
 export function setCurrentDialRotation(rotation) {
+    console.log("rotation", rotation)
     currentDialRotation = rotation
+    let normalizedAngle = -currentDialRotation % 360
+    if (normalizedAngle < 0) {
+        normalizedAngle += 360
+    }
+    effectiveKnobAngleOnDialScale = normalizedAngle
 }
