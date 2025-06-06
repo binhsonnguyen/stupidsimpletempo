c6d7f11ec9eba6d17c07caf74a8f52cef7ffebc2
son <son@codegym.vn>
2025-06-06 10:28:33 +0700

    .

--- generate-version.js
+++ generate-version.js
@@ -6,7 +6,7 @@ const packageJson = require('./package.json'); // Đọc tệp package.json
 const version = packageJson.version;
 // Tạo nội dung cho tệp version.js, ví dụ: export const APP_VERSION = "1.0.0";
 const content = `export const APP_VERSION = "${version}";
-console.log("APP_VERSION: " + version);`;
+console.log("APP_VERSION: " + APP_VERSION);`;
 
 // Đường dẫn đến nơi lưu tệp version.js (ví dụ: bên trong thư mục js/)
 // Đảm bảo đường dẫn này đúng với cấu trúc thư mục của bạn
