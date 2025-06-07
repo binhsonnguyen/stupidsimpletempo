f5b456a1bd8cd0eedfe3731ded2da2989f2e0a7a
son <son@codegym.vn>
2025-06-07 22:04:57 +0700

    .

--- src/application/state.js
+++ src/application/state.js
@@ -1,7 +1,7 @@
-import { MIN_SCALE_BPM, ANGLE_FOR_MIN_SCALE_BPM_MARK } from '../infrastructure/config.js'
-import {logger} from "../infrastructure/logger";
+import * as config from '../infrastructure/config.js'
+import { logger } from "../infrastructure/logger.js"
 
-export let currentBpm = MIN_SCALE_BPM
+export let currentBpm = config.MIN_SCALE_BPM
 export let isMetronomeRunning = false
 export let currentDialRotation = 0
 export let effectiveKnobAngleOnDialScale = 0
@@ -9,7 +9,7 @@ export let wakeLockSentinel = null
 
 export function setCurrentBpm(newBpm) {
     const clampedBpm = Math.max(config.MIN_SCALE_BPM, Math.min(config.MAX_SCALE_BPM, newBpm))
-    currentBpm = newBpm
+    currentBpm = clampedBpm
 }
 
 export function setIsMetronomeRunning(running) {
