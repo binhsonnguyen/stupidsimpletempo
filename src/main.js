b62af0b8055fdc161e6b9e5fe20d25ed0ba9b96b
son <son@codegym.vn>
2025-06-14 22:27:46 +0700

    .

--- src/main.js
+++ src/main.js
@@ -9,23 +9,52 @@ function initializeApp () {
     } = dependencies
 
     audioService.initializeAudioContext();
+    function isMobile() {
+        const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+        return regex.test(navigator.userAgent);
+    }
 
     const unlockAudioContext = (ctx) => {
         return new Promise((resolve) => {
             const b = document.body;
             const events = ["touchstart", "touchend", "mousedown", "keydown"];
-            const unlock = () => { ctx.resume().then(clean).then(resolve) };
+            const unlock = () => {
+                if (ctx.state === 'suspended') {
+                    ctx.resume().then(clean).then(resolve);
+                } else {
+                    clean()
+                    resolve()
+                }
+            };
             const clean = () => { events.forEach(e => b.removeEventListener(e, unlock));};
             events.forEach(e => b.addEventListener(e, unlock, false));
         })
     }
 
+    if (!isMobile()) {
+        unlockAudioContext(audioService.getAudioContext()).then(() => {})
+    }
+
     // Tạo và gán instance của StartButton
     dependencies.startButton = new components.StartButton({
         element: dom.startStopButtonElement,
-        onTap: () => unlockAudioContext(audioService.getAudioContext())
-            .then(() => controller.handleButtonTap({ useCases, presenter, audioService }))
-    })
+        onTap: () => {
+            if (isMobile()) {
+                unlockAudioContext(audioService.getAudioContext())
+                    .then(() => controller.handleButtonTap({
+                        useCases,
+                        presenter,
+                        audioService
+                    }));
+            } else {
+                controller.handleButtonTap({
+                    useCases,
+                    presenter,
+                    audioService
+                });
+            }
+        }
+    });
 
     // Tạo và gán instance của Dial
     dependencies.dial = new components.Dial({
