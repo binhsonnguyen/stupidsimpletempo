2fe8fb1715dfb91c89238109459f1795d62b297b
son <son@codegym.vn>
2025-06-20 21:57:19 +0700

    .

--- generate-version.js
+++ generate-version.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const packageJson = require('./package.json');
-const {logger} = require("./src/infrastructure/logger.ts");
 
 const version = packageJson.version;
 const content = `export const APP_VERSION = "${version}";\n`;
@@ -9,4 +8,3 @@ const content = `export const APP_VERSION = "${version}";\n`;
 const outputPath = path.join(__dirname, 'src', 'version.js');
 
 fs.writeFileSync(outputPath, content, 'utf8');
-logger.log(`Đã tạo tệp ${path.basename(outputPath)} tại ${path.dirname(outputPath)} với phiên bản ${version}`);
