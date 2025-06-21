a7ec108b5a8dd4f5a1734897aa0f102ca5198fcf
son <son@codegym.vn>
2025-06-21 15:44:06 +0700

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- src/routes/+page.svelte -->
+<script>
+    import Header from '$lib/components/Header.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Header />
+
+<main class="flex-grow-1 d-flex align-items-center justify-content-center">
+    <div class="container text-center">
+        <h1 class="display-1">120</h1>
+        <p class="lead">BPM</p>
+    </div>
+</main>
+
+<Footer />
