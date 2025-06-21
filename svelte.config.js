7837ed382ba8fa9f97ab5953cf1880686654eaf4
son <son@codegym.vn>
2025-06-21 14:50:25 +0700

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+// svelte.config.js
+
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			// mọi yêu cầu đến server (ví dụ: khi người dùng F5 trang ở đường dẫn /about)
+			// sẽ được chuyển về index.html
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
