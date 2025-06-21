6dcc23606b2912e60584b2e830bb0d6f04b78d6a
son <son@codegym.vn>
2025-06-21 15:43:47 +0700

    .

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<!-- src/lib/components/Footer.svelte -->
+<script>
+	const currentYear = new Date().getFullYear();
+</script>
+
+<footer class="mt-auto text-center bg-black bg-opacity-10 app-footer">
+	Stupid Simple Tempo ©{currentYear} Nguyen Binh Son
+</footer>
+
+<style>
+	.app-footer {
+		padding: 10px 0;
+		color: #6c757d;
+		font-size: 0.75em;
+	}
+</style>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<!-- src/lib/components/Header.svelte -->
+
+<header class="d-flex justify-content-center app-header">
+    <img
+            class="app-logo"
+            src="/images/logo.png"
+            alt="Stupid Simple Tempo Logo"
+    />
+</header>
+
+<style>
+    .app-header {
+        padding: 15px 0;
+    }
+
+    .app-logo {
+        filter: invert(1);
+        mix-blend-mode: screen;
+        max-height: 40px;
+        width: auto;
+    }
+</style>
