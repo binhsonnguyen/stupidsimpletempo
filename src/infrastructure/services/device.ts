bbdc92afaa7ab4d2b617a890d5d479468ad54760
son <son@codegym.vn>
2025-06-21 13:39:05 +0700

    .

--- src/infrastructure/services/device.ts
+++
@@ -1,8 +0,0 @@
-export function isMobile() {
-    const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-    return regex.test(navigator.userAgent);
-}
-
-export function isDesktop() {
-    return !isMobile()
-}
