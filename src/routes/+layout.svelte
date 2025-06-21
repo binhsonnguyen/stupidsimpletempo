f96f1890c8ece43ce9b2b1ae3e1a793ea4c6a01d
son <son@codegym.vn>
2025-06-21 14:21:29 +0700

    .

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+    import '../styles/theme.scss';
+</script>
+
+<slot />
+
+<style global>
+    html, body {
+        height: 100%;
+        margin: 0;
+        overscroll-behavior: none;
+        touch-action: none;
+    }
+
+    body {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        display: flex;
+        flex-direction: column;
+        min-height: 100dvh;
+    }
+
+</style>
