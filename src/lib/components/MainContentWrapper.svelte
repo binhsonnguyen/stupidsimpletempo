5df32c738a9fdaacef525d143ec84fd419f0a3e0
son <son@codegym.vn>
2025-06-22 21:27:57 +0700

    .

---
+++ src/lib/components/MainContentWrapper.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<!-- src/lib/components/MainContentWrapper.svelte -->
+
+
+<div class="container text-center">
+    <slot />
+</div>
+
+<style>
+    /*
+       Bạn có thể thêm bất kỳ style tùy chỉnh nào cho wrapper này nếu cần.
+       Hiện tại, không cần style cụ thể nào ở đây vì Bootstrap đã xử lý hầu hết.
+    */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,19 @@
 <script>
     import Header from '$lib/components/Header.svelte';
     import Footer from '$lib/components/Footer.svelte';
+    import MainContentWrapper from "$lib/components/MainContentWrapper.svelte";
 </script>
 
 <Header />
 
+<!--d-flex: Biến main thành một flex container-->
+<!--align-items-center: Căn giữa các phần tử con của main theo chiều dọc (cross-axis)-->
+<!--justify-content-center: Căn giữa các phần tử con của main theo chiều ngang (main-axis).-->
 <main class="flex-grow-1 d-flex align-items-center justify-content-center">
-    <div class="container text-center">
+    <MainContentWrapper>
         <h1 class="display-1">120</h1>
         <p class="lead">BPM</p>
-    </div>
+    </MainContentWrapper>
 </main>
 
 <Footer />
