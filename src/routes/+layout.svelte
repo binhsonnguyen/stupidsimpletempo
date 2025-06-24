41edd111de996fff9fb3d84c2a84a15dc5628508
son <son@codegym.vn>
2025-06-25 00:37:16 +0700

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,15 @@
 
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
-	import { tick } from 'svelte'; // 1. Import tick
+	import { tick } from 'svelte';
 	import { Sound } from '$lib/audio/Sound';
 	import { isAudioLoading } from '$lib/state/audioLoadingStore';
+	import { wakeLockService } from '$lib/services/wakeLockService'; // 1. Import service
 	import '../styles/theme.scss';
 
 	onMount(async () => {
+		wakeLockService.initialize();
+
 		// Đợi một "tick" để đảm bảo tất cả các onMount của component con đã chạy
 		// và đã đăng ký xong âm thanh của chúng.
 		await tick();
@@ -21,7 +24,6 @@
 		Sound.disposeAll();
 	});
 </script>
-
 
 <svelte:body class:audio-loading={$isAudioLoading} />
 
