52e8204f6e62b5ebc3700ce9ad565be8b7776849
son <son@codegym.vn>
2025-06-25 16:37:41 +0700

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,21 @@
 	import { tick } from 'svelte';
 	import { Sound } from '$lib/audio/Sound';
 	import { isAudioLoading } from '$lib/state/audioLoadingStore';
-	import { wakeLockService } from '$lib/services/wakeLockService'; // 1. Import service
+	import { wakeLockService } from '$lib/services/wakeLockService';
+	import { metronomeStore } from '$lib/state/metronomeStore';
+	import { beatSequenceStore } from '$lib/state/beatSequenceStore';
 	import '../styles/theme.scss';
+
+	let unsubscribeMetronome: (() => void) | undefined;
 
 	onMount(async () => {
 		wakeLockService.initialize();
+
+		// Đăng ký beatSequenceStore với metronomeStore
+		// Việc này đảm bảo cả hai store đã được khởi tạo trước khi đăng ký
+		unsubscribeMetronome = metronomeStore.subscribe(({ beatsPerMeasure }) => {
+			beatSequenceStore.setSequence(beatsPerMeasure);
+		});
 
 		// Đợi một "tick" để đảm bảo tất cả các onMount của component con đã chạy
 		// và đã đăng ký xong âm thanh của chúng.
@@ -22,6 +32,9 @@
 
 	onDestroy(() => {
 		Sound.disposeAll();
+		if (unsubscribeMetronome) {
+			unsubscribeMetronome();
+		}
 	});
 </script>
 
