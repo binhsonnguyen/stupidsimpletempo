cf11cfe8384f0fbd03b3f6e0c7349855d0f3d6e6
son <son@codegym.vn>
2025-06-25 23:17:44 +0700

    .

--- src/lib/components/time-notation-selector/TimeNotationSelector.svelte
+++ src/lib/components/time-notation-selector/TimeNotationSelector.svelte
@@ -1,23 +1,23 @@
 <!-- src/lib/components/time-notation-selector/TimeNotationSelector.svelte -->
 <script lang="ts">
-	const noteOptions = {
-		WHOLE_NOTE: { label: '1', value: '1n', description: 'Whole Note (Nốt tròn)' },
+	const BEAT_UNITS = {
+		WHOLE_NOTE: { label: '1m', value: '1m', description: 'Whole Note (Nốt tròn)' },
 		HALF_NOTE: { label: '2', value: '2n', description: 'Half Note (Nốt trắng)' },
 		QUARTER_NOTE: { label: '4', value: '4n', description: 'Quarter Note (Nốt đen)' },
 		EIGHTH_NOTE: { label: '8', value: '8n', description: 'Eighth Note (Nốt móc đơn)' },
 		SIXTEENTH_NOTE: { label: '16', value: '16n', description: 'Sixteenth Note (Nốt móc kép)' },
-		EIGHTH_TRIPLET: { label: '8³', value: '8t', description: 'Eighth Triplet (Chùm ba nốt móc đơn)' }
+		EIGHTH_TRIPLET: { label: '8t', value: '8t', description: 'Eighth Triplet (Chùm ba nốt móc đơn)' }
 	};
 
-	const displayNotes = [noteOptions.HALF_NOTE, noteOptions.QUARTER_NOTE, noteOptions.EIGHTH_NOTE];
+	const BEAT_UNIT_OPTIONS = [BEAT_UNITS.HALF_NOTE, BEAT_UNITS.QUARTER_NOTE, BEAT_UNITS.EIGHTH_NOTE];
 </script>
 
 <div class="notation-wrapper">
 	<div class="division-line"></div>
 	<div class="time-notation-selector-container">
-		{#each displayNotes as note}
-			<span class="note-symbol" title={note.description}>
-				{@html note.label}
+		{#each BEAT_UNIT_OPTIONS as bu (bu.value)}
+			<span class="note-symbol" title={bu.description}>
+				{bu.label}
 			</span>
 		{/each}
 	</div>
@@ -48,9 +48,8 @@
     .note-symbol {
         font-size: 1em;
         cursor: pointer;
-        transition:
-                color 0.2s ease,
-                transform 0.2s ease;
+        transition: color 0.2s ease,
+        transform 0.2s ease;
     }
 
     .note-symbol:hover {
