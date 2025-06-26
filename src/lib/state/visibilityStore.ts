ae848067a37421f3077c221eb11bfdc2d89c0de1
son <son@codegym.vn>
2025-06-26 16:29:50 +0700

    .

---
+++ src/lib/state/visibilityStore.ts
@@ -0,0 +1,33 @@
+// src/lib/components/time-notation-selector/visibilityStore.ts
+import { writable } from 'svelte/store';
+
+const HIDE_DELAY = 2000;
+
+function createVisibilityStore() {
+	const { subscribe, set } = writable(true);
+	let hideTimer: number;
+
+	function startHideTimer() {
+		clearTimeout(hideTimer);
+		hideTimer = window.setTimeout(() => {
+			set(false);
+		}, HIDE_DELAY);
+	}
+
+	function cancelHideTimer() {
+		clearTimeout(hideTimer);
+	}
+
+	return {
+		subscribe,
+		show: () => {
+			cancelHideTimer();
+			set(true);
+			startHideTimer();
+		},
+		startHideTimer,
+		cancelHideTimer
+	};
+}
+
+export const filmStripVisibilityStore = createVisibilityStore();
