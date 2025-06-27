cf07d589ff130cfb958d6303bbc58b76ad2631e3
son <son@codegym.vn>
2025-06-28 00:19:32 +0700

    .

--- src/lib/config/chromaConstants.ts
+++ src/lib/config/chromaConstants.ts
@@ -1,8 +1,13 @@
 // src/lib/config/chromaConstants.ts
 
-export const COLOR_ON_RGB = '220, 220, 220';
-export const COLOR_OFF_RGB = '40, 167, 69';
-export const COLOR_LOADING_RGB = '108, 117, 125';
+export const COLOR_WHITE_RGB = '220, 220, 220';
+export const COLOR_RED_RGB = '220, 53, 69';
+export const COLOR_GREEN_RGB = '40, 167, 69';
+export const COLOR_GRAY_RGB = '108, 117, 125';
+
+export const COLOR_ON_RGB = COLOR_WHITE_RGB;
+export const COLOR_OFF_RGB = COLOR_GREEN_RGB;
+export const COLOR_LOADING_RGB = COLOR_GRAY_RGB;
 
 export const GLOW_SPREAD_STATIC = 0; // px - Độ lan rộng khi metronome tắt
 export const GLOW_SPREAD_PULSE_MIN = 0; // px - Độ lan rộng tối thiểu khi đang chạy
