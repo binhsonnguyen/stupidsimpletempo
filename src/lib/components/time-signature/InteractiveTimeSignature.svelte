6bd1d0d34bab2c8a4f7a5ab17e0c1ff4ea6978c6
son <son@codegym.vn>
2025-06-28 22:38:37 +0700

    .

--- src/lib/components/time-signature/InteractiveTimeSignature.svelte
+++ src/lib/components/time-signature/InteractiveTimeSignature.svelte
@@ -5,12 +5,16 @@
 	import { fade } from 'svelte/transition';
 	import { swipeable } from '$lib/components/actions/swipeable';
 	import { userInteractionStore } from '$lib/state/userInteractionFeedbackStore';
+	import { drumGlowStore } from '$lib/state/drumGlowStore';
 	import { VALID_BEAT_INTERVALS, VALID_DIVISIONS, type BeatInterval, type Division } from '$lib/constants';
 	import type { TimeSignature } from '$lib/models/TimeSignature';
 
 	let { timeSignature } = $props<{
 		timeSignature: TimeSignature;
 	}>();
+
+	const TINT_INTENSITY_FACTOR = 0.8;
+	const tintIntensity = $derived($drumGlowStore.baseIntensity * TINT_INTENSITY_FACTOR);
 
 	const dispatch = createEventDispatcher<{
 		changeBeats: Division;
@@ -57,90 +61,129 @@
 	<span class="time-signature-fraction">{timeSignature.fraction}</span>
 
 	{#if $userInteractionStore}
-		<div class="interaction-cues" transition:fade={{ duration: 150 }}>
-			<div class="arrow arrow-up"></div>
-			<div class="arrow arrow-down"></div>
-			<div class="arrow arrow-left"></div>
-			<div class="arrow arrow-right"></div>
+		<div
+			class="interaction-cues"
+			transition:fade={{ duration: 150 }}
+			style="--glow-rgb: {$drumGlowStore.rgb}; --tint-intensity: {tintIntensity};"
+		>
+			<div class="arrow arrow-up">
+				<div class="arrow-line"></div>
+				<div class="arrow-line"></div>
+			</div>
+			<div class="arrow arrow-down">
+				<div class="arrow-line"></div>
+				<div class="arrow-line"></div>
+			</div>
+			<div class="arrow arrow-left">
+				<div class="arrow-line"></div>
+				<div class="arrow-line"></div>
+			</div>
+			<div class="arrow arrow-right">
+				<div class="arrow-line"></div>
+				<div class="arrow-line"></div>
+			</div>
 		</div>
 	{/if}
 </div>
 
-<style>
-    .time-signature-container {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        cursor: grab;
-        user-select: none;
-        padding: 8px 16px;
-        border-radius: 8px;
-        transition: background-color 0.2s,
-        transform 0.2s ease-out;
-        min-height: 60px;
-        min-width: 60px;
-    }
+<style lang="scss">
+  .time-signature-container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    cursor: grab;
+    user-select: none;
+    padding: 8px 16px;
+    border-radius: 8px;
+    transition: background-color 0.2s,
+    transform 0.2s ease-out;
+    min-height: 60px;
+    min-width: 60px;
+  }
 
-    .time-signature-container:active {
-        cursor: grabbing;
-    }
+  .time-signature-container:active {
+    cursor: grabbing;
+  }
 
-    .time-signature-fraction {
-        font-size: 1.4rem;
-        font-weight: lighter;
-        line-height: 1;
-        color: #dee2e6;
-        font-variant-numeric: tabular-nums;
-        transition: color 0.2s ease-out,
-        transform 0.2s ease-out;
-    }
+  .time-signature-fraction {
+    font-size: 1.4rem;
+    font-weight: lighter;
+    line-height: 1;
+    color: #dee2e6;
+    font-variant-numeric: tabular-nums;
+    transition: color 0.2s ease-out,
+    transform 0.2s ease-out;
+  }
 
-    .time-signature-container.is-interacting .time-signature-fraction {
-        color: white;
-        transform: scale(1.1);
-    }
+  .time-signature-container.is-interacting .time-signature-fraction {
+    color: white;
+    transform: scale(1.1);
+  }
 
-    .interaction-cues {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        pointer-events: none;
-        --arrow-offset-from-center: 28px;
-    }
+  .interaction-cues {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    --arrow-offset-from-center: 38px;
+  }
 
-    .arrow {
-        position: absolute;
-        width: 6px;
-        height: 6px;
-        border: solid white;
-        border-width: 0 1px 1px 0;
-    }
+  .arrow-line {
+    position: absolute;
+    width: 10px;
+    height: 1px;
+    top: 50%;
+    left: 50%;
+    transform-origin: 0 50%;
 
-    .arrow-up {
-        top: calc(50% - var(--arrow-offset-from-center));
-        left: 50%;
-        transform: translate(-50%, -50%) rotate(-135deg);
-    }
+    $base-color: #6c757d;
+    background: linear-gradient(
+                    to right,
+                    color-mix(in srgb, rgba(var(--glow-rgb), 1) calc(var(--tint-intensity) * 100%), $base-color),
+                    transparent
+    );
+    transition: background 0.1s ease;
+  }
 
-    .arrow-down {
-        top: calc(50% + var(--arrow-offset-from-center));
-        left: 50%;
-        transform: translate(-50%, -50%) rotate(45deg);
-    }
+  .arrow {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+  }
 
-    .arrow-left {
-        left: calc(50% - var(--arrow-offset-from-center));
-        top: 50%;
-        transform: translate(-50%, -50%) rotate(135deg);
-    }
+  .arrow-up {
+    top: calc(50% - var(--arrow-offset-from-center));
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(90deg);
+  }
 
-    .arrow-right {
-        left: calc(50% + var(--arrow-offset-from-center));
-        top: 50%;
-        transform: translate(-50%, -50%) rotate(-45deg);
-    }
+  .arrow-down {
+    top: calc(50% + var(--arrow-offset-from-center));
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(-90deg);
+  }
+
+  .arrow-left {
+    left: calc(50% - var(--arrow-offset-from-center));
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .arrow-right {
+    left: calc(50% + var(--arrow-offset-from-center));
+    top: 50%;
+    transform: translate(-50%, -50%) rotate(180deg);
+  }
+
+  .arrow .arrow-line:nth-child(1) {
+    transform: rotate(45deg);
+  }
+
+  .arrow .arrow-line:nth-child(2) {
+    transform: rotate(-45deg);
+  }
 </style>
