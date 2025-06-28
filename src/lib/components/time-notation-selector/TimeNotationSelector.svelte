182eaa912994bee404efda2910244d6a6e971399
son <son@codegym.vn>
2025-06-28 21:22:45 +0700

    .

--- src/lib/components/time-notation-selector/TimeNotationSelector.svelte
+++ src/lib/components/time-notation-selector/TimeNotationSelector.svelte
@@ -1,47 +1,24 @@
 <!-- src/lib/components/time-notation-selector/TimeNotationSelector.svelte -->
 <script lang="ts">
 	import { metronomeStore } from '$lib/state/metronomeStore';
-	import type { BeatInterval } from '$lib/constants';
-	import TimeNotationFilmStrip from './TimeNotationFilmStrip.svelte';
-	import type { BeatIntervalOption } from './TimeNotationFilmStrip.svelte';
-	import { makeFraction } from '$lib/utils/fractionGenerator';
+	import type { BeatInterval, Division } from '$lib/constants';
+	import InteractiveTimeSignature from './InteractiveTimeSignature.svelte';
 
-	const beatIntervalToDenominator: Record<BeatInterval, number> = {
-		'1m': 1,
-		'2n': 2,
-		'4n': 4,
-		'8n': 8,
-		'16n': 16,
-		'8t': 8
-	};
+	function handleChangeBeats(event: CustomEvent<Division>) {
+		metronomeStore.setBeatsPerMeasure(event.detail);
+	}
 
-	const BASE_OPTIONS: Omit<BeatIntervalOption, 'timeSignatureLabel'>[] = [
-		{ label: '⚬', value: '1m', description: 'Whole Note (Nốt tròn)', symbol: '⚬' },
-		{ label: '½', value: '2n', description: 'Half Note (Nốt trắng)', symbol: '𝅗𝅥' },
-		{ label: '¼', value: '4n', description: 'Quarter Note (Nốt đen)', symbol: '𝅘𝅥' },
-		{ label: '⅛', value: '8n', description: 'Eighth Note (Nốt móc đơn)', symbol: '𝅘𝅥𝅮' },
-		{ label: '¹⁄₁₆', value: '16n', description: 'Sixteenth Note (Nốt móc kép)', symbol: '𝅘𝅥𝅯' },
-		{ label: '⅛³', value: '8t', description: 'Eighth Triplet (Chùm ba)', symbol: '𝅘𝅥𝅮³' }
-	];
-
-	$: optionsWithTimeSignature = BASE_OPTIONS.map((option) => {
-		const denominator = beatIntervalToDenominator[option.value] || 0;
-		return {
-			...option,
-			timeSignatureLabel: makeFraction($metronomeStore.beatsPerMeasure, denominator)
-		};
-	});
-
-	function handleSelectionChange(event: CustomEvent<BeatInterval>) {
+	function handleChangeInterval(event: CustomEvent<BeatInterval>) {
 		metronomeStore.setBeatInterval(event.detail);
 	}
 </script>
 
 <div class="notation-wrapper">
-	<TimeNotationFilmStrip
-		options={optionsWithTimeSignature}
-		initialValue={$metronomeStore.beatInterval}
-		on:change={handleSelectionChange}
+	<InteractiveTimeSignature
+		beatsPerMeasure={$metronomeStore.beatsPerMeasure}
+		beatInterval={$metronomeStore.beatInterval}
+		on:changeBeats={handleChangeBeats}
+		on:changeInterval={handleChangeInterval}
 	/>
 </div>
 
@@ -51,5 +28,6 @@
         flex-direction: column;
         align-items: center;
         width: 100%;
+        margin-bottom: 10px;
     }
 </style>
