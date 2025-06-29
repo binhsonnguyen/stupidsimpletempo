a3a2b6b5c4b57429baf3c6d951fa1affe45b2fc8
son <son@codegym.vn>
2025-06-29 21:27:04 +0700

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,27 @@
 <!-- src/routes/+page.svelte -->
-<script>
+<script lang="ts">
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import MainContentWrapper from '$lib/components/MainContentWrapper.svelte';
 	import Dial from '$lib/components/dial/Dial.svelte';
 	import TimeSignatureSwitcher from '$lib/components/time-signature/TimeSignatureSwitcher.svelte';
+
+	const pageTitle = 'Tempo';
+	const pageDescription =
+		'Stupid Simple Tempo - A Free Online Metronome - A clean, simple, and accurate online metronome for musicians. Set tempo, time signature, and subdivisions with ease. No ads, no clutter. Just tempo.';
+	const canonicalUrl = 'https://stupidsimpletempo.com';
 </script>
+
+<svelte:head>
+	<title>{pageTitle}</title>
+	<meta name="description" content={pageDescription} />
+	<link rel="canonical" href={canonicalUrl} />
+	<meta property="og:title" content="Stupid Simple Tempo" />
+	<meta property="og:description" content={pageDescription} />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={canonicalUrl} />
+	 <meta property="og:image" content="{canonicalUrl}/images/preview-image.png" />
+</svelte:head>
 
 <Header />
 
