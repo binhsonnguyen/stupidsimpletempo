e173ecf93637049b10e36c6216fad59326265d2f
son <son@codegym.vn>
2025-06-29 15:03:14 +0700

    .

--- src/lib/state/chromaStore.ts
+++ src/lib/state/chromaStore.ts
@@ -19,31 +19,40 @@ export type GlowState = RawGlowState & {
 	baseIntensity: number;
 };
 
-const rawGlowStore = writable<RawGlowState>({
-	rgb: COLOR_OFF_RGB,
-	alpha: GLOW_ALPHA_STATIC,
-	spread: GLOW_SPREAD_STATIC
-});
+function createGlowStore(initialState: RawGlowState) {
+	const rawGlowStore = writable<RawGlowState>(initialState);
 
-const derivedGlowStore = derived<typeof rawGlowStore, GlowState>(
-	rawGlowStore,
-	($rawGlow) => {
+	const derivedGlowStore = derived<typeof rawGlowStore, GlowState>(rawGlowStore, ($rawGlow) => {
 		const adjustedSpread = $rawGlow.spread + SPREAD_OFFSET;
 		const adjustedMaxSpread = GLOW_SPREAD_PULSE_MAX + SPREAD_OFFSET;
 		const spreadFactor = adjustedMaxSpread > 0 ? adjustedSpread / adjustedMaxSpread : 0;
-
 		const calculatedIntensity = spreadFactor * $rawGlow.alpha;
 
 		return {
 			...$rawGlow,
 			baseIntensity: calculatedIntensity
 		};
-	}
-);
+	});
 
-export const chromaStore = {
-	subscribe: derivedGlowStore.subscribe,
-	setGlow: (rawState: RawGlowState) => {
-		rawGlowStore.set(rawState);
-	}
-};
+	return {
+		subscribe: derivedGlowStore.subscribe,
+		setGlow: (rawState: RawGlowState) => {
+			rawGlowStore.set(rawState);
+		},
+		turnOff: () => {
+			rawGlowStore.set(initialState);
+		}
+	};
+}
+
+export const drumGlowStore = createGlowStore({
+	rgb: COLOR_OFF_RGB,
+	alpha: GLOW_ALPHA_STATIC,
+	spread: GLOW_SPREAD_STATIC
+});
+
+export const dialGlowStore = createGlowStore({
+	rgb: COLOR_OFF_RGB,
+	alpha: GLOW_ALPHA_STATIC,
+	spread: GLOW_SPREAD_STATIC
+});
