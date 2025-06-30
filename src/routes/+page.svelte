1e7a0b96639d723a2ec124c8ac32d6cd694b6f8b
son <son@codegym.vn>
2025-06-30 23:36:10 +0700

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
 	<meta property="og:description" content={pageDescription} />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content={canonicalUrl} />
-	<link rel="preload" href="/images/logo.png" as="image" fetchpriority="high" />
+	<link rel="preload" href="/images/logo.webp" as="image" fetchpriority="high" />
 	<meta property="og:image" content="{canonicalUrl}/images/preview.png" />
 </svelte:head>
 
