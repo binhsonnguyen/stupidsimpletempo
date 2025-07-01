8aadba4e393919d2f460794944d24b64fa1bfb92
son <son@codegym.vn>
2025-07-01 08:36:56 +0700

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,14 @@
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import MainContentWrapper from '$lib/components/MainContentWrapper.svelte';
-	import { onMount } from 'svelte';
+
+	import Dial from '$lib/components/dial/Dial.svelte';
+	import TimeSignatureSwitcher from '$lib/components/time-signature/TimeSignatureSwitcher.svelte';
 
 	const pageTitle = 'Tempo';
 	const pageDescription =
 		'Stupid Simple Tempo - A Free Online Metronome - A clean, simple, and accurate online metronome for musicians. Set tempo, time signature, and subdivisions with ease. No ads, no clutter. Just tempo.';
 	const canonicalUrl = 'https://stupidsimpletempo.com';
-
-	let Dial: unknown;
-	let TimeSignatureSwitcher: unknown;
-
-	onMount(async () => {
-		Dial = (await import('$lib/components/dial/Dial.svelte')).default;
-		TimeSignatureSwitcher = (
-			await import('$lib/components/time-signature/TimeSignatureSwitcher.svelte')
-		).default;
-	});
 </script>
 
 <svelte:head>
@@ -38,18 +30,9 @@
 <main class="flex-grow-1 d-flex align-items-center justify-content-center">
 	<MainContentWrapper>
 		<div class="content-stack">
-			{#if Dial}
-				<svelte:component this={Dial} />
-			{:else}
-				<div style="height: 300px; width: 300px;"></div>
-			{/if}
-
+			<Dial />
 			<div class="notation-container">
-				{#if TimeSignatureSwitcher}
-					<svelte:component this={TimeSignatureSwitcher} />
-				{:else}
-					<div style="height: 60px;"></div>
-				{/if}
+				<TimeSignatureSwitcher />
 			</div>
 		</div>
 	</MainContentWrapper>
