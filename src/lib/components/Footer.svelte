afa414b97ae5f93168d6b088f3686ff03c308e7e
son <son@codegym.vn>
2025-07-01 22:01:21 +0700

    .

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,9 @@
 <!-- src/lib/components/Footer.svelte -->
 
 <script lang="ts">
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+	import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
+
 	const version = __APP_VERSION__;
 	const author = __APP_AUTHOR__;
 
@@ -8,7 +11,10 @@
 </script>
 
 <footer class="mt-auto text-center bg-black bg-opacity-10 app-footer">
-	v{version} &copy;{currentYear} {author}
+	<span>v{version} &copy;{currentYear} {author}</span>
+	<a href="/settings" class="settings-link" aria-label="Application Settings">
+		<FontAwesomeIcon icon={faScrewdriverWrench} />
+	</a>
 </footer>
 
 <style lang="scss">
@@ -20,5 +26,28 @@
     font-size: 0.75em;
     user-select: text;
     -webkit-user-select: text;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+
+  .settings-link {
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+
+    color: inherit;
+    font-size: 1.3em;
+    opacity: 0.6;
+    transition: opacity 0.2s ease-in-out,
+    transform 0.2s ease-in-out;
+
+    &:hover {
+      opacity: 1;
+      transform: translateY(-50%) rotate(15deg);
+    }
   }
 </style>
